2ce22bb81709637969746c5f789b39ed8396429e
marekryb96 <marekrybicki96@gmail.com>
2017-09-25 00:41:34 +0200

    problem z przełączaniem utworów

---
+++ display.h
@@ -0,0 +1,11 @@
+#include "stm32f4xx_tim.h"
+
+#ifndef display_h
+#define display_h
+
+int abs(int l);
+float absf(float l);
+double scale_time();
+void Display_Real_Number(double number);
+
+#endif
